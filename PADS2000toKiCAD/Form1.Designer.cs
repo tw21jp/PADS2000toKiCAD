7736c5afea2046b6fb87b6f257367dfb846af0c4
wada <you@example.com>
2023-10-03 16:29:42 +0900

    first added

---
+++ PADS2000toKiCAD/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.PartsListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(88, 42);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(362, 184);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(567, 42);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(362, 184);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(88, 298);
+            this.richTextBox3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(362, 184);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(963, 42);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "開始";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // PartsListBox
+            // 
+            this.PartsListBox.FormattingEnabled = true;
+            this.PartsListBox.ItemHeight = 21;
+            this.PartsListBox.Location = new System.Drawing.Point(567, 298);
+            this.PartsListBox.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.PartsListBox.Name = "PartsListBox";
+            this.PartsListBox.Size = new System.Drawing.Size(217, 151);
+            this.PartsListBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(84, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ネットリスト";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(84, 271);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "部品表";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(563, 16);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "出力";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(563, 271);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "パーツ名一覧";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 569);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PartsListBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox PartsListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PADS2000toKiCAD/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private List<string> ExtractPartNumbers(string inputText)
+        {
+            List<string> partNumbers = new List<string>();
+
+            // カンマで分割し、3つ目以降の要素をパーツ番号として抽出
+            string[] text = inputText.Split('\n');
+            foreach (string e in text)
+            {
+                string[] parts = e.Split(',');
+                for (int i = 2; i < parts.Length; i++)
+                {
+                    string part = parts[i].Trim();
+                    // パーツ番号を正規表現で抽出
+                    Match match = Regex.Match(part, @"\w+\d+");
+                    if (match.Success)
+                    {
+                        partNumbers.Add(match.Value);
+                    }
+                }
+            }
+
+            return partNumbers;
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string inputText = richTextBox3.Text;
+            List<string> partNumbers = ExtractPartNumbers(inputText);
+
+            // リストボックスにパーツ番号を表示
+            PartsListBox.Items.Clear();
+            PartsListBox.Items.AddRange(partNumbers.ToArray());
+            //出力用データ
+            StringBuilder kicadNetlist = new StringBuilder();
+            //タイトル　日付は適当
+            kicadNetlist.AppendLine("( { EESchema Netlist Version 1.1 created 2023/10/03 11:10:05 }");
+            //パーツごとにループR1,R2・・・・
+            foreach (string f in partNumbers)
+            {
+                //パーツのネットを作成。部品と値は空に設定
+                kicadNetlist.AppendLine($" ( /00000000-0000-0000-0000-000000000000 parts  {f} 0");
+
+                //ネットリストを読み込み
+                using (StringReader reader = new StringReader(richTextBox1.Text))
+                {
+                    string line;
+                    string currentSignal = null;
+                    //ネットリスト一行読み
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        //シグナルがあればシグナル名取得
+                        if (line.StartsWith("*SIGNAL*"))
+                        {
+                            currentSignal = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[1];
+                        }
+                        //それ以外はパーツ検索
+                        else if (!string.IsNullOrWhiteSpace(line))
+                        {
+                            string[] components = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                            if (components.Length > 0 )
+                            {
+                                foreach (string component in components)
+                                {
+                                    //パーツが一致すれば、ネットリストに登録
+                                    string[] parts = component.Split('.');
+                                    if (parts[0] == f)
+                                    {
+                                        kicadNetlist.AppendLine($"  (    {parts[1]} {currentSignal} )");
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+                kicadNetlist.AppendLine(" )");
+            }
+
+            richTextBox2.Text = kicadNetlist.ToString();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
